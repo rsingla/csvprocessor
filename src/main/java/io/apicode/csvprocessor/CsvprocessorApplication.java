7e8d0737c26b49096a1a9d32a0eeb30e4bb6f9ed
Rajeev Singla <rajeev1.singla@gmail.com>
2021-07-02 00:09:05 -0700

    Added POM and project structure

---
+++ src/main/java/io/apicode/csvprocessor/CsvprocessorApplication.java
@@ -0,0 +1,13 @@
+package io.apicode.csvprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvprocessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/apicode/csvprocessor/CsvprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.apicode.csvprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
